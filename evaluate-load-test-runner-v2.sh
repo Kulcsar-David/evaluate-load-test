58b8c448c39ccfc47a0684795c9054f6ebec0e9d
David Kulcsar <dkulcsar@precognox.com>
2022-08-28 21:58:09 +0200

    feat: all file paths updated

--- evaluate-load-test-runner-v2.sh
+++ evaluate-load-test-runner-v2.sh
@@ -5,18 +5,18 @@ export JVM_ARGS="-Dnashorn.args=--no-deprecation-warning"
 
 # <run the test>
 
-./jmeter -n -t first-wave_0-5-in-5.jmx
-./jmeter -n -t second-wave_keep-5-in-5.jmx
-./jmeter -n -t third-wave_5-10-in-5.jmx
-./jmeter -n -t fourth-wave_keep-10-in-5.jmx
-./jmeter -n -t fifth-wave_keep-5-in-5.jmx
+./jmeter -n -t ../test-files/first-wave_0-5-in-5.jmx
+./jmeter -n -t ../test-files/second-wave_keep-5-in-5.jmx
+./jmeter -n -t ../test-files/third-wave_5-10-in-5.jmx
+./jmeter -n -t ../test-files/fourth-wave_keep-10-in-5.jmx
+./jmeter -n -t ../test-files/fifth-wave_keep-5-in-5.jmx
 
 # <generating html reports>
 
-./jmeter -g ../results/summary-report-log-1.csv -o ../result_output/summary-report-log-1_0to5in5
-./jmeter -g ../results/summary-report-log-2.csv -o ../result_output/summary-report-log-2_keep5in5
-./jmeter -g ../results/summary-report-log-3.csv -o ../result_output/summary-report-log-3_5to10in5
-./jmeter -g ../results/summary-report-log-4.csv -o ../result_output/summary-report-log-4_keep10in5
-./jmeter -g ../results/summary-report-log-5.csv -o ../result_output/summary-report-log-5_keep5in5
+./jmeter -g /opt/apache-jmeter-5.5/bin/result_output/summary-report-log-1.csv -o /opt/apache-jmeter-5.5/bin/result_output/log-1_0to5in5
+./jmeter -g /opt/apache-jmeter-5.5/bin/result_output/summary-report-log-2.csv -o /opt/apache-jmeter-5.5/bin/result_output/log-2_keep5in5
+./jmeter -g /opt/apache-jmeter-5.5/bin/result_output/summary-report-log-3.csv -o /opt/apache-jmeter-5.5/bin/result_output/log-3_5to10in5
+./jmeter -g /opt/apache-jmeter-5.5/bin/result_output/summary-report-log-4.csv -o /opt/apache-jmeter-5.5/bin/result_output/log-4_keep10in5
+./jmeter -g /opt/apache-jmeter-5.5/bin/result_output/summary-report-log-5.csv -o /opt/apache-jmeter-5.5/bin/result_output/log-5_keep5in5
 
 # ./JMeterPluginsCMD.sh --generate-csv test.csv --input-jtl results.jtl --plugin-type ResponseTimesOverTime
