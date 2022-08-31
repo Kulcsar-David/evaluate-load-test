adfe629953eb3a25db8b81fa88be0eb846feacd0
David Kulcsar <dkulcsar@precognox.com>
2022-08-31 07:46:19 +0200

    feat: final

--- evaluate-load-test/evaluate-load-test-runner-v2.sh
+++ evaluate-load-test/evaluate-load-test-runner-v2.sh
@@ -7,18 +7,18 @@ export JVM_ARGS="-Dnashorn.args=--no-deprecation-warning"
 
 # <run the test>
 
-$HOME/jmeter -n -t evaluate-load-test/first-wave_0-5-in-5.jmx
-$HOME/jmeter -n -t evaluate-load-test/second-wave_keep-5-in-5.jmx
-$HOME/jmeter -n -t evaluate-load-test/third-wave_5-10-in-5.jmx
-$HOME/jmeter -n -t evaluate-load-test/fourth-wave_keep-10-in-5.jmx
-$HOME/jmeter -n -t evaluate-load-test/fifth-wave_keep-5-in-5.jmx
+$HOME/jmeter -n -t evaluate-load-test/1st-wave-0-5-in-5.jmx
+$HOME/jmeter -n -t evaluate-load-test/2nd-wave-keep-5-in-5.jmx
+$HOME/jmeter -n -t evaluate-load-test/3rd-wave-0-10-in-5.jmx
+$HOME/jmeter -n -t evaluate-load-test/4th-wave-keep-10-in-5.jmx
+$HOME/jmeter -n -t evaluate-load-test/5th-wave-0-10-in-10.jmx
 
 # <generating html reports>
 
 $HOME/jmeter -g $HOME/result_output/summary-report-log-1.csv -o $HOME/result_output/summary-report-log-1_0to5in5
 $HOME/jmeter -g $HOME/result_output/summary-report-log-2.csv -o $HOME/result_output/summary-report-log-2_keep5in5
-$HOME/jmeter -g $HOME/result_output/summary-report-log-3.csv -o $HOME/result_output/summary-report-log-3_5to10in5
+$HOME/jmeter -g $HOME/result_output/summary-report-log-3.csv -o $HOME/result_output/summary-report-log-3_0to10in5
 $HOME/jmeter -g $HOME/result_output/summary-report-log-4.csv -o $HOME/result_output/summary-report-log-4_keep10in5
-$HOME/jmeter -g $HOME/result_output/summary-report-log-5.csv -o $HOME/result_output/summary-report-log-5_keep5in5
+$HOME/jmeter -g $HOME/result_output/summary-report-log-5.csv -o $HOME/result_output/summary-report-log-5_0to10in10
 
 # ./JMeterPluginsCMD.sh --generate-csv test.csv --input-jtl results.jtl --plugin-type ResponseTimesOverTime
